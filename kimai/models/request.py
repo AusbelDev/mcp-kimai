115c76e25a8c8d338e26a966580668d9f8ad97bd
Ausbel <ausbel.dev@gmail.com>
2025-10-22 11:01:37 -0600

    oops fixed one merge conflict I didnt see

--- kimai/models/request.py
+++ kimai/models/request.py
@@ -51,55 +51,28 @@ class IKimaiFetchRecentTimesheetsParams(BaseModel):
     size: Optional[int] = None
 
 
-  @field_serializer('begin')
-  def to_html5_date(self, value) -> str:
-    return value.strftime("%Y-%m-%dT%H:%M:%S")
-
 class IKimaiFetchTimesheetsParams(BaseModel):
-<<<<<<< HEAD
-  user: Optional[str] = None
-  customers: Optional[List[str]] = None
-  projects: Optional[List[str]] = None
-  activities: Optional[List[str]] = None
-  page: Optional[int] = None # 1
-  size: Optional[int] = None # 50
-  tags: Optional[str] = None
-  orderBy: Optional[OrderByOptions] = None # "name"
-  order: Optional[OrderDirectionOptions] = None # "ASC"
-  begin: Optional[datetime] = None
-  end: Optional[datetime] = None
-  active: Optional[bool] = None
-  billable: Optional[bool] = None
-  full: Optional[bool] = None # False
-  term: Optional[str] = None
-  modified_after: Optional[datetime] = None
-
-  @field_serializer('begin', 'end', 'modified_after')
-  def to_html5_date(self, value) -> str:
-    return value.strftime("%Y-%m-%dT%H:%M:%S")
-
-  @field_serializer('customers', 'projects', 'activities')
-  def join_lists(self, value) -> str:
-    return ",".join(value)
-=======
     user: Optional[str] = None
     customers: Optional[List[str]] = None
     projects: Optional[List[str]] = None
     activities: Optional[List[str]] = None
-    page: Optional[int] = 1
-    size: Optional[int] = 50
+    page: Optional[int] = None  # 1
+    size: Optional[int] = None  # 50
     tags: Optional[str] = None
-    orderBy: Optional[OrderByOptions] = "name"
-    order: Optional[OrderDirectionOptions] = "ASC"
+    orderBy: Optional[OrderByOptions] = None  # "name"
+    order: Optional[OrderDirectionOptions] = None  # "ASC"
     begin: Optional[datetime] = None
     end: Optional[datetime] = None
     active: Optional[bool] = None
     billable: Optional[bool] = None
-    full: Optional[bool] = False
+    full: Optional[bool] = None  # False
     term: Optional[str] = None
     modified_after: Optional[datetime] = None
+
+    @field_serializer("begin", "end", "modified_after")
+    def to_html5_date(self, value) -> str:
+        return value.strftime("%Y-%m-%dT%H:%M:%S")
 
     @field_serializer("customers", "projects", "activities")
     def join_lists(self, value) -> str:
         return ",".join(value)
->>>>>>> main
